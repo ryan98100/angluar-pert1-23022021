b68f26a0948364d066d55de55977d793536eda79
ryan98100 <c14180115@john.petra.ac.id>
2021-02-23 18:16:12 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  a = 0;
+  b = 0;
+  item = {
+    nama: "kelengkeng",
+    harga: 10000
+  };
+  crypto = [
+    {
+      nama: "Cardano",
+      harga: 13000
+    },
+    {
+      nama: "LiteCoin",
+      harga: 2700000
+    }
+  ];
+  tampil: boolean = false;
+  pesan = "";
+  itemArr = [1, 2, 3, 4];
+  OPEN() {
+    this.pesan = "button is clicked";
+    alert("DIBUKA");
+  }
+
+  value = "";
+  onKey(event: any) {
+    this.value += event.target.value + " | ";
+  }
 }
